8a5b0b4e2a9343bbaf81cd7a828332f6000f6c74
matheus <matisschmidt@gmail.com>
2025-01-29 20:49:00 -0300

    Adição de stg funcionários

---
+++ models/intermediate/int_vendas__join_funcionarios.sql
@@ -0,0 +1,20 @@
+WITH funcionarios as (
+    SELECT *
+    from {{ ref('stg_erp__funcionarios') }}
+)
+
+, join_funcionarios as (
+    SELECT 
+        funcionarios.codigo_funcionario
+	    , funcionarios.nome_funcionario
+	    , funcionarios.cargo_funcionario
+	    , funcionarios.codigo_gerente
+        , funcionarios.data_nascimento
+        , funcionarios.data_contratacao
+        , funcionarios.cidade_funcionario
+        , funcionarios.regiao_funcionario
+        , funcionarios.pais_funcionario
+    FROM funcionarios
+    LEFT JOIN funcionarios as gerentes
+         ON funcionarios.codigo_funcionario = gerentes.codigo_funcionario
+)
